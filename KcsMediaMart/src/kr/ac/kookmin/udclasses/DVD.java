8eaa4053e67a8a5fc590307ec098ef5d786777ed
minwoogood12 <84243851+minwoogood12@users.noreply.github.com>
2021-10-19 21:52:08 +0900

    Add files via upload
    객체지향프로그래밍 Lab07-2 KcsMediaMart 파일 올리기 과제

---
+++ KcsMediaMart/src/kr/ac/kookmin/udclasses/DVD.java
@@ -0,0 +1,49 @@
+package kr.ac.kookmin.udclasses;
+
+public class DVD {
+	private String title;
+	private String category;
+	private String director;
+	private int length;
+	private float cost;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getDirector() {
+		return director;
+	}
+
+	public void setDirector(String director) {
+		this.director = director;
+	}
+
+	public int getLength() {
+		return length;
+	}
+
+	public void setLength(int length) {
+		this.length = length;
+	}
+
+	public float getCost() {
+		return cost;
+	}
+
+    public void setCost(float cost) {
+	    this.cost = cost;
+    }
+}    
